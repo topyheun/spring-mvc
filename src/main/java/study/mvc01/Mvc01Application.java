3c1ce1d015e48b1cfb779df868b597307e0a3fa8
Topy <gmsdl1994@naver.com>
2022-09-29 18:46:35 +0900

    first commit

---
+++ src/main/java/study/mvc01/Mvc01Application.java
@@ -0,0 +1,13 @@
+package study.mvc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc01Application.class, args);
+	}
+
+}
---
+++ src/test/java/study/mvc01/Mvc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.mvc01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
